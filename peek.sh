67964e15a9d46f30716c02facecf29d8a4079476
Aina <ainavpico@gmail.com>
2023-11-01 18:34:51 +0100

    Second modification

--- peek.sh
+++ peek.sh
@@ -1,3 +1,2 @@
-head -n $2 $1
-echo ...
-tail -n $2 $1
+if [[ -z $2 ]]; then var=3; else var=$2; fi
+if [[ $(wc -l < $1) -le $((2*$var)) ]]; then cat $1; else echo Warning: Too many lines; head -n $var $1; echo ...; tail -n $var $1; fi
